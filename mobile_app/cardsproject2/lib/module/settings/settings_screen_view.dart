4f94c175d1b4fdd30ef88ab0baa2d9ede26798b9
seif <saifaboashour98@gmail.com>
2023-05-29 21:01:32 +0300

    feat: Main Settings Screen Design
    - Icons missing

--- mobile_app/cardsproject2/lib/module/settings/settings_screen_view.dart
+++ mobile_app/cardsproject2/lib/module/settings/settings_screen_view.dart
@@ -6,6 +6,7 @@ import 'package:get/get.dart';
 import '../../util/images_path.dart';
 import '../../view/custom_header.dart';
 import 'settings_controller.dart';
+import 'view/settings_button.dart';
 
 class SettingsScreen extends StatelessWidget {
   SettingsScreen({
@@ -17,16 +18,23 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: [
-          CustomHeader(
-            lable: 'Settings',
-            icon: ImagePath.settings,
-            action: () {},
-            actionIcon: null,
-          ),
-          buildUserInfoCard(),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            CustomHeader(
+              lable: 'Settings',
+              icon: ImagePath.settings,
+              action: () {},
+              actionIcon: null,
+            ),
+            buildUserInfoCard(),
+            buildAccountSettingsSection(),
+            buildCreditSettingsSection(),
+            buildAppSettingsSection(),
+            buildHelpSection(),
+            buildLogoutSection(),
+          ],
+        ),
       ),
     );
   }
@@ -58,6 +66,138 @@ class SettingsScreen extends StatelessWidget {
               ),
             ],
           ),
+        ],
+      ),
+    );
+  }
+
+  buildAccountSettingsSection() {
+    return Padding(
+      padding: EdgeInsets.all(Get.width * 0.02),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Account',
+            style:
+                TextStyles.captionLarge.copyWith(fontWeight: FontWeight.bold),
+          ),
+          SettingsButton(
+            title: 'Edit Profile',
+            icon: ImagePath.home,
+            onTap: () {},
+          ),
+          SettingsButton(
+            title: 'Change Password',
+            icon: ImagePath.home,
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  buildCreditSettingsSection() {
+    return Padding(
+      padding: EdgeInsets.all(Get.width * 0.02),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Credit',
+            style:
+                TextStyles.captionLarge.copyWith(fontWeight: FontWeight.bold),
+          ),
+          SettingsButton(
+            title: 'Request Credit',
+            icon: ImagePath.home,
+            onTap: () {},
+          ),
+          SettingsButton(
+            title: 'Credit History',
+            icon: ImagePath.home,
+            onTap: () {},
+          ),
+          SettingsButton(
+            title: 'Change Currency',
+            icon: ImagePath.home,
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  buildAppSettingsSection() {
+    return Padding(
+      padding: EdgeInsets.all(Get.width * 0.02),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'App Settings',
+            style:
+                TextStyles.captionLarge.copyWith(fontWeight: FontWeight.bold),
+          ),
+          SettingsButton(
+            title: 'Change Language',
+            icon: ImagePath.home,
+            onTap: () {},
+          ),
+          SettingsButton(
+            title: 'Manage Printers',
+            icon: ImagePath.home,
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  buildHelpSection() {
+    return Padding(
+      padding: EdgeInsets.all(Get.width * 0.02),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Help',
+            style:
+                TextStyles.captionLarge.copyWith(fontWeight: FontWeight.bold),
+          ),
+          SettingsButton(
+            title: 'Support',
+            icon: ImagePath.home,
+            onTap: () {},
+          ),
+          SettingsButton(
+            title: 'Privacy Policy',
+            icon: ImagePath.home,
+            onTap: () {},
+          ),
+          SettingsButton(
+            title: 'Terms & Conditions',
+            icon: ImagePath.home,
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  buildLogoutSection() {
+    return Padding(
+      padding: EdgeInsets.all(Get.width * 0.02),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CommonWidgets().buildVerticalSpace(space: 0.06),
+          SettingsButton(
+            title: 'logout',
+            icon: ImagePath.home,
+            onTap: () {},
+          ),
+          CommonWidgets().buildVerticalSpace(space: 0.02),
         ],
       ),
     );
