ab995f9b65ab173f6f34dd10ec14f10e2ed07144
seif <saifaboashour98@gmail.com>
2023-05-29 18:51:55 +0300

    feat: Settings User Info

--- mobile_app/cardsproject2/lib/module/settings/settings_screen_view.dart
+++ mobile_app/cardsproject2/lib/module/settings/settings_screen_view.dart
@@ -1,3 +1,5 @@
+import 'package:cardsproject2/util/common_widgets.dart';
+import 'package:cardsproject2/util/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -22,6 +24,39 @@ class SettingsScreen extends StatelessWidget {
             icon: ImagePath.settings,
             action: () {},
             actionIcon: null,
+          ),
+          buildUserInfoCard(),
+        ],
+      ),
+    );
+  }
+
+  buildUserInfoCard() {
+    return Padding(
+      padding: EdgeInsets.all(Get.width * 0.02),
+      child: Row(
+        children: [
+          Container(
+            height: Get.width * 0.25,
+            width: Get.width * 0.25,
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(Get.width * 0.01),
+            ),
+          ),
+          CommonWidgets().buildHorizontalSpace(space: 0.02),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Bitaqty Store',
+                style: TextStyles.captionLarge,
+              ),
+              Text(
+                'saifaboashour@bitaqty.com',
+                style: TextStyles.captionSmall,
+              ),
+            ],
           ),
         ],
       ),
